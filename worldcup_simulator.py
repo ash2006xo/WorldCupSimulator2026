c850e51c5069e4420c3ff1790630ecd5d66c8928
Ash2006xo <narges.army.1385@gmail.com>
2026-07-04 17:04:18 +0330

    Create project skeleton

---
+++ worldcup_simulator.py
@@ -0,0 +1,32 @@
+# ============================================================
+# Student : Narges Farhangi                       
+# Student ID : [ 404131043 ]                      
+# Project : World Cup Simulator 2026
+# Date : *****
+# ============================================================
+
+# ===================== IMPORTS ==============================
+import csv
+import random
+import numpy as np
+import math
+
+#===================== CONSTANTS =============================
+NUMBER_OF_TEAMS = 32
+NUMBER_OF_GROUPS = 8
+TEAMS_PER_GROUP = 4
+GROUP_STAGE_POINTS_WIN = 3
+GROUP_STAGE_POINTS_DRAW = 1
+MATCH_DURATION = 90
+EXTRA_TIME_DURATION = 30
+PENALTY_ATTEMPTS = 5
+
+
+# =================== HELPER FUNCTIONS =======================
+
+
+
+# ===================== TEAM CLASS ===========================
+
+
+
